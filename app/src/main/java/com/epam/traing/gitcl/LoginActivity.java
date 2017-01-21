4484e1854fe00e8ac61faeeff80f0bec311ea423
Yahor Fralou <Yahor_Fralou@epam.com>
2017-01-21 18:08:27 +0300

    Minor styles changes; Go to MainActivity by clicking on 'Not that time'

--- app/src/main/java/com/epam/traing/gitcl/LoginActivity.java
+++ app/src/main/java/com/epam/traing/gitcl/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.epam.traing.gitcl;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.txtSkipLogin)
+    TextView txtSkipLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        txtSkipLogin.setPaintFlags(txtSkipLogin.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    @OnClick(R.id.txtSkipLogin)
+    void onSkipLoginClick() {
+        // TODO remove this activity from backlog
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
     }
 }
