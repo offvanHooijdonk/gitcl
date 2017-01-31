25346b0bcb242d15f585babf60c7c185906a1a72
Yahor Fralou <Yahor_Fralou@epam.com>
2017-01-31 19:40:53 +0300

    Experiments with animation; Curtain activity removed; Colors experiments; Logo drawable changed.

--- app/src/main/java/com/epam/traing/gitcl/ui/LoginActivity.java
+++ app/src/main/java/com/epam/traing/gitcl/ui/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.epam.traing.gitcl.ui;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,11 +31,22 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
         skip, login_new, logged_in
     }
 
+    boolean isNextScreenStarted = false;
+
     @Inject
     ILoginPresenter loginPresenter;
 
+
+    @Bind(R.id.editLayoutLogin)
+    TextInputLayout editLayoutLogin;
+    @Bind(R.id.editLayoutPassword)
+    TextInputLayout editLayoutPassword;
+    @Bind(R.id.btnLogin)
+    Button btnLogin;
     @Bind(R.id.txtSkipLogin)
     TextView txtSkipLogin;
+    @Bind(R.id.imgLogoAnim)
+    ImageView imgLogoAnim;
 
     private ProgressDialog progressDialog;
 
@@ -39,6 +58,51 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
         loginPresenter.setView(this);
 
         ButterKnife.bind(this);
+
+        editLayoutLogin.setAlpha(0f);
+        editLayoutPassword.setAlpha(0f);
+        btnLogin.setAlpha(0f);
+        txtSkipLogin.setAlpha(0f);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!isNextScreenStarted) {
+            animateRevealLogin();
+            isNextScreenStarted = true;
+        }
+    }
+
+    private void animateRevealLogin() {
+        float marginTopStart = getResources().getDimensionPixelSize(R.dimen.login_logo_vert_margin_start);
+        float marginTopEnd = getResources().getDimensionPixelSize(R.dimen.login_logo_vert_margin);
+        float sizeStart = getResources().getDimensionPixelSize(R.dimen.login_logo_anim_size);
+        float sizeEnd = getResources().getDimensionPixelSize(R.dimen.login_logo_size);
+        float logoEndScale = sizeEnd/sizeStart;
+
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(imgLogoAnim, View.SCALE_X, 1, logoEndScale);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(imgLogoAnim, View.SCALE_Y, 1, logoEndScale);
+        ObjectAnimator moveY = ObjectAnimator.ofFloat(imgLogoAnim, View.Y, marginTopStart, marginTopEnd);
+        ObjectAnimator fadeEditLogin = ObjectAnimator.ofFloat(editLayoutLogin, View.ALPHA, 1f);
+        ObjectAnimator fadeEditPassword = ObjectAnimator.ofFloat(editLayoutPassword, View.ALPHA, 1f);
+        ObjectAnimator fadeBtnLogin = ObjectAnimator.ofFloat(btnLogin, View.ALPHA, 1f);
+        ObjectAnimator fadeTxtSkipLogin = ObjectAnimator.ofFloat(txtSkipLogin, View.ALPHA, 1f);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setInterpolator(new AccelerateInterpolator());
+        set.setDuration(500);
+
+        set.play(scaleX)
+                .with(scaleY)
+                .with(moveY)
+                .before(fadeEditLogin)
+                .before(fadeEditPassword)
+                .before(fadeBtnLogin)
+                .before(fadeTxtSkipLogin);
+
+        new Handler().postDelayed(set::start, 750);
     }
 
     @OnClick(R.id.btnLogin)
@@ -96,15 +160,18 @@ public class LoginActivity extends AppCompatActivity implements ILoginView {
             case skip: {
                 animEnter = R.anim.activity_slide_rl_enter;
                 animLeave = R.anim.activity_slide_rl_leave;
-            } break;
+            }
+            break;
             case login_new: {
                 animEnter = R.anim.activity_slide_rl_enter;
                 animLeave = R.anim.activity_slide_rl_leave;
-            } break;
+            }
+            break;
             case logged_in: {
                 animEnter = android.R.anim.fade_in;
                 animLeave = android.R.anim.fade_out;
-            } break;
+            }
+            break;
             default: {
                 animEnter = android.R.anim.fade_in;
                 animLeave = android.R.anim.fade_out;
